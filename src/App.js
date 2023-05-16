ca0c8e07b6fcb7a792420c75557f0bbb4f44b9bf
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-16 23:13:00 +0300

    add font

--- src/App.js
+++ src/App.js
@@ -49,15 +49,17 @@ function App() {
     return (
         <div className='bg-gray-800 h-screen grid place-items-center'>
             <div className='bg-white dark:bg-slate-800 w-96 p-4 rounded-md'>
-                <div className='flex items-center justify-between'>
-                    <input type="text"
-                           ref={inputRef}
-                           placeholder="Enter Your Location"
-                           className='text-xl border-b p-1 border-gray-200 font-semibold uppercase flex-1'
-                    />
-                    <button onClick={fetchWeather}>
+                <div className="flex">
+                    <input type="text" id="website-admin"
+                                                      ref={inputRef}
+
+                           className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-xl border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="ادخل موقعك"/>
+
+                    <button onClick={fetchWeather} className='inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600'>
                         <img src="https://cdn-icons-png.flaticon.com/512/758/758651.png" alt="" className='w-8'/>
                     </button>
+
                 </div>
 
                 <div className={`duration-300 delay-75 overflow-hidden ${showWeather ? 'h-[27rem]' : 'h-0'} `}>
@@ -76,15 +78,16 @@ function App() {
 
                                 <img src={showWeather[0]?.img} alt="" className='w-52 mx-auto'/>
                                 <h3 className='text-2xl font-bold'>
-                                    {showWeather[0]?.type}
+                                    {apiData?.weather[0]?.description}
                                 </h3>
 
 
                                 {
                                     apiData && (<div className='flex justify-center'>
-                                        <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
-                                             className='h-9 mt-1'/>
+
                                         <h2 className='text-4xl font-extrabold'>{apiData?.main?.temp}&#176;C</h2>
+                                      <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
+                                             className='h-9 mt-1'/>
                                     </div>)
                                 }
 
@@ -93,8 +96,6 @@ function App() {
                         )
                     }
                 </div>
-
-
             </div>
         </div>
     )
--- src/data/remote/urls.js
+++ src/data/remote/urls.js
@@ -5,7 +5,7 @@ const apiKey = '60989022778c254dd43568777b92b238';
 const baseUrl = 'https://api.openweathermap.org/';
 const apiUrl = `${baseUrl}data/2.5/weather`;
 
- const search = (q) => `${apiUrl}?q=${q}&units=metric&appid=${apiKey}`;
+ const search = (q) => `${apiUrl}?q=${q}&units=metric&lang=ar&appid=${apiKey}`;
 
 const weatherTypes = [
     {
