762832b2d944eb60299bb16d68333269697922e1
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-17 12:43:25 +0300

    add icons

--- src/App.js
+++ src/App.js
@@ -51,12 +51,13 @@ function App() {
             <div className='bg-white dark:bg-slate-800 w-96 p-4 rounded-md'>
                 <div className="flex">
                     <input type="text" id="website-admin"
-                                                      ref={inputRef}
-
+                           ref={inputRef}
+                           onChange={fetchWeather}
                            className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-xl border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder="ادخل موقعك"/>
 
-                    <button onClick={fetchWeather} className='inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600'>
+                    <button onClick={fetchWeather}
+                            className='inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600'>
                         <img src="https://cdn-icons-png.flaticon.com/512/758/758651.png" alt="" className='w-8'/>
                     </button>
 
@@ -78,7 +79,7 @@ function App() {
 
                                 <img src={showWeather[0]?.img} alt="" className='w-52 mx-auto'/>
                                 <h3 className='text-2xl font-bold'>
-                                    {apiData?.weather[0]?.description}
+                                    {apiData?.weather[0]?.description ?? 'لا توجد بيانات'}
                                 </h3>
 
 
@@ -86,7 +87,7 @@ function App() {
                                     apiData && (<div className='flex justify-center'>
 
                                         <h2 className='text-4xl font-extrabold'>{apiData?.main?.temp}&#176;C</h2>
-                                      <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
+                                        <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
                                              className='h-9 mt-1'/>
                                     </div>)
                                 }
