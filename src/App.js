b5028423990b6d8b24696184a1a9c9f352c0355c
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-17 18:03:59 +0300

    convert input to component

--- src/App.js
+++ src/App.js
@@ -1,105 +1,86 @@
 import React, {useRef, useState} from 'react';
 import * as urls from "./data/remote/urls";
+import Input from "./components/Input";
 
 function App() {
 
-    const [apiData, setApiData] = useState(null);
-    const [showWeather, setShowWeather] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [apiData, setApiData] = useState(null);
+  const [showWeather, setShowWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const inputRef = useRef(null);
-    const fetchWeather = async () => {
-        setLoading(true);
-        const url = urls.search(inputRef.current.value);
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                setApiData(null);
-                if (data.cod === '404' || data.cod === '400') {
-                    setShowWeather(
-                        [
-                            {
-                                type: "Not Found",
-                                img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
-                            }
-                        ]
-                    )
-                }
-                setShowWeather(
-                    urls.weatherTypes.filter(
-                        (weather) => weather.type === data.weather[0].main
-                    )
-                );
-                console.log(data);
-                setApiData(data);
+  const inputRef = useRef(null);
+  const fetchWeather = async () => {
+    setLoading(true);
+    const url = urls.search(inputRef.current.value);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setApiData(null);
+          if (data.cod === '404' || data.cod === '400') {
+            setShowWeather(
+                [
+                  {
+                    type: "Not Found",
+                    img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
+                  }
+                ]
+            )
+          }
+          setShowWeather(
+              urls.weatherTypes.filter(
+                  (weather) => weather.type === data.weather[0].main
+              )
+          );
+          console.log(data);
+          setApiData(data);
+        })
+        .catch(
+            (error => (error) => {
+              // setShowWeather(
+              //     [
+              //         {
+              //             type: "Not Found",
+              //             img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
+              //         }
+              //     ]
+              // )
             })
-            .catch(
-                (error => (error) => {
-                    // setShowWeather(
-                    //     [
-                    //         {
-                    //             type: "Not Found",
-                    //             img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
-                    //         }
-                    //     ]
-                    // )
-                })
-            ).finally(() => setLoading(false));
-    };
-    return (
-        <div className='bg-secondary h-screen grid place-items-center'>
-            <div className='bg-white dark:bg-slate-800 w-96 p-4 rounded-md'>
-                <div className="flex">
-                    <input type="text"
-                           ref={inputRef}
-                           onChange={fetchWeather}
-                           className="focus:outline-primary  focus:ring-primary focus:border-primary rounded-l-0 focus:rounded-l-0 focus:outline-0 rounded-r-lg bg-gray-50 border text-gray-900  w-full text-xl border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
-                           placeholder="ادخل موقعك"/>
-
-                    <button onClick={fetchWeather}
-                            className='inline-flex items-center px-3 text-sm text-gray-900 bg-primary border-none border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600'>
-                        <img src="https://cdn-icons-png.flaticon.com/512/758/758651.png" alt="" className='w-8'/>
-                    </button>
-
-                </div>
-
-                <div className={`duration-300 delay-75 overflow-hidden ${showWeather ? 'h-[27rem]' : 'h-0'} `}>
+        ).finally(() => setLoading(false));
+  };
+  return (
+      <div className='bg-secondary h-screen justify-center flex items-center   px-8'>
+        <div className='bg-white drop-shadow-xl dark:bg-slate-800  w-96 p-4 rounded-md'>
+          <Input fetchWeather={fetchWeather} inputRef={inputRef}/>
+          <div className={`duration-300 delay-75 overflow-hidden ${showWeather ? 'h-[27rem]' : 'h-0'} `}>
+            {
+              loading ? <div className='grid place-items-center h-full'><img
+                  src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png" alt=""
+                  className='w-14 mx-auto mb-2 animate-spin'
+              />
+              </div> : (
+                  showWeather && (<div className='text-center flex flex-col gap-6 mt-10'>
                     {
-                        loading ? <div className='grid place-items-center h-full'><img
-                            src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png" alt=""
-                            className='w-14 mx-auto mb-2 animate-spin'
-                        />
-
-                        </div> : (
-                            showWeather && (<div className='text-center flex flex-col gap-6 mt-10'>
-                                {
-                                    apiData && (
-                                        <p className='text-xl text-gray-900 font-semibold'>{apiData?.name + ', ' + apiData?.sys?.country} </p>)
-                                }
-
-                                <img src={showWeather[0]?.img} alt="" className='w-52 mx-auto'/>
-                                <h3 className='text-2xl text-gray-900  font-bold'>
-                                    {apiData?.weather[0]?.description ?? 'لا توجد بيانات'}
-                                </h3>
-
-
-                                {
-                                    apiData && (<div className='flex justify-center'>
-
-                                        <h2 className='text-4xl text-gray-900 font-extrabold'>{apiData?.main?.temp}&#176;C</h2>
-                                        <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
-                                             className='h-9 mt-1'/>
-                                    </div>)
-                                }
-
-
-                            </div>)
-                        )
+                        apiData && (
+                            <p className='text-xl text-gray-900 font-semibold'>{apiData?.name + ', ' + apiData?.sys?.country} </p>)
                     }
-                </div>
-            </div>
+                    <img src={showWeather[0]?.img} alt="" className='w-52 mx-auto'/>
+                    <h3 className='text-2xl text-gray-900  font-bold'>
+                      {apiData?.weather[0]?.description ?? 'لا توجد بيانات'}
+                    </h3>
+                    {
+                        apiData && (<div className='flex justify-center'>
+                          <h2 className='text-4xl text-gray-900 font-extrabold'>{apiData?.main?.temp}&#176;C</h2>
+                          <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
+                               className='h-9 mt-1'/>
+                        </div>)
+                    }
+                  </div>)
+              )
+            }
+          </div>
         </div>
-    )
+      </div>
+  )
 }
 
 export default App;
--- src/data/remote/urls.js
+++ src/data/remote/urls.js
@@ -4,7 +4,7 @@ const apiKey = '60989022778c254dd43568777b92b238';
 const baseUrl = 'https://api.openweathermap.org/';
 const apiUrl = `${baseUrl}data/2.5/weather`;
 
- const search = (q) => `${apiUrl}?q=${q}&units=metric&lang=ar&appid=${apiKey}`;
+ const search = (q) => `${apiUrl}?q=${q.trim()}&units=metric&lang=ar&appid=${apiKey}`;
 
 const weatherTypes = [
     {
