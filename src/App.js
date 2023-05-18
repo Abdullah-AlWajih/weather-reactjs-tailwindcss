eada14f7158d32812ad967e665b4c27fbf3db405
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-18 17:36:59 +0300

    check network and handle error

--- src/App.js
+++ src/App.js
@@ -1,85 +1,106 @@
 import React, {useRef, useState} from 'react';
 import * as urls from "./data/remote/urls";
+import * as images from "./data/remote/images";
 import Input from "./components/Input";
 
 function App() {
 
     const [apiData, setApiData] = useState(null);
-    const [showWeather, setShowWeather] = useState(null);
+    // const [showWeather, setShowWeather] = useState(null);
     const [loading, setLoading] = useState(false);
 
     const inputRef = useRef(null);
-    const fetchWeather = async () => {
-        const successCallback = (position) => {
-            console.log(position, 'sss');
-        };
 
-        const errorCallback = (error) => {
-            console.log(error, 'eee');
-        };
-
-        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-        setLoading(true);
-        const url = urls.search(inputRef.current.value);
-        // fetch(url)
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         setApiData(null);
-        //         if (data.cod === '404' || data.cod === '400') {
-        //             setShowWeather(
-        //                 [
-        //                     {
-        //                         type: "Not Found",
-        //                         img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
-        //                     }
-        //                 ]
-        //             )
-        //         }
-        //         setShowWeather(
-        //             urls.weatherTypes.filter(
-        //                 (weather) => weather.type === data.weather[0].main
-        //             )
-        //         );
-        //         console.log(data);
-        //         setApiData(data);
-        //     })
-        //     .catch(
-        //         (error => (error) => {
-        //             // setShowWeather(
-        //             //     [
-        //             //         {
-        //             //             type: "Not Found",
-        //             //             img: "https://cdn-icons-png.flaticon.com/512/4275/4275497.png",
-        //             //         }
-        //             //     ]
-        //             // )
-        //         })
-        //     ).finally(() => setLoading(false));
+    const successCallback = async (position) => {
+        const url = urls.current(position.coords.latitude, position.coords.longitude);
+        await fetchWeather(url);
     };
+
+    const errorCallback = async (error) => {
+        const url = urls.search(inputRef.current.value);
+        await fetchWeather(url);
+    };
+
+    const searchWeather = async () => {
+        setLoading(true);
+        if (inputRef.current.value.trim() === '') {
+            await navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        } else {
+            const url = urls.search(inputRef.current.value);
+            await fetchWeather(url);
+        }
+    }
+
+    const fetchWeather = async (url) => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.cod === '404' || data.cod === '400') {
+                    setApiData(
+                        {
+                            type: "Not Found",
+                            description: 'لا توجد بيانات',
+                            img: images.weatherTypes.notFound,
+                        }
+                    )
+                    console.log(apiData);
+                } else {
+                    setApiData(
+                        {
+                            description: data?.weather[0]?.description,
+                            temp: data?.main?.temp,
+                            type: data.weather[0].type,
+                            name: data?.name,
+                            country: data?.sys?.country,
+                            img: images.weatherTypes[data.weather[0].main],
+                        }
+                    )
+                }
+
+                // setApiData(data);
+            })
+            .catch((error) => {
+                    // setApiData(null);
+
+                    if (error.message === 'Failed to fetch') {
+                        setApiData(
+                            {
+                                type: "لا يوجد اتصال بالشبكة",
+                                description: 'لا يوجد اتصال بالشبكة',
+                                img: images.weatherTypes.noWifi,
+                            }
+                        )
+
+                    }
+                }
+            ).finally(() => setLoading(false));
+    };
+
+
     return (
         <div className='bg-secondary min-h-screen justify-center flex items-center   px-8'>
-            <div className='bg-gray-800 drop-shadow-xl dark:bg-slate-800  w-96 p-4 rounded-md'>
-                <Input fetchWeather={fetchWeather} inputRef={inputRef}/>
-                <div className={`duration-300 delay-75 overflow-hidden ${showWeather ? 'h-[27rem]' : 'h-0'} `}>
+            <div className='bg-white drop-shadow-xl dark:bg-slate-800  w-96 p-4 rounded-md'>
+                <Input searchWeather={searchWeather} inputRef={inputRef}/>
+                <div className={`duration-300 delay-75 overflow-hidden ${apiData || loading ? 'h-[27rem]' : 'h-0'} `}>
                     {
                         loading ? <div className='grid place-items-center h-full'><img
-                            src="https://cdn-icons-png.flaticon.com/512/1477/1477009.png" alt=""
+                            src={images.weatherTypes.loading} alt=""
                             className='w-14 mx-auto mb-2 animate-spin'
                         />
                         </div> : (
-                            showWeather && (<div className='text-center flex flex-col gap-6 mt-10'>
+                            apiData && (<div className='text-center flex flex-col gap-6 mt-10'>
                                 {
-                                    apiData && (
-                                        <p className='text-xl text-gray-900 font-semibold'>{apiData?.name + ', ' + apiData?.sys?.country} </p>)
+                                    apiData?.name && (
+                                        <p className='text-xl text-gray-900 font-semibold'>{apiData?.name + ', ' + apiData?.country} </p>)
                                 }
-                                <img src={showWeather[0]?.img} alt="" className='w-52 mx-auto'/>
+                                <img src={apiData?.img} alt="" className='w-52 mx-auto'/>
                                 <h3 className='text-2xl text-gray-900  font-bold'>
-                                    {apiData?.weather[0]?.description ?? 'لا توجد بيانات'}
+                                    {apiData?.description}
                                 </h3>
                                 {
-                                    apiData && (<div className='flex justify-center'>
-                                        <h2 className='text-4xl text-gray-900 font-extrabold'>{apiData?.main?.temp}&#176;C</h2>
-                                        <img src="https://cdn-icons-png.flaticon.com/512/7794/7794499.png" alt=""
+                                    apiData?.temp && (<div className='flex justify-center'>
+                                        <h2 className='text-4xl text-gray-900 font-extrabold'>{apiData?.temp}&#176;C</h2>
+                                        <img src={images.weatherTypes.temp} alt=""
                                              className='h-9 mt-1'/>
                                     </div>)
                                 }
---
+++ src/data/remote/images.js
@@ -0,0 +1,32 @@
+import clear from '../../assets/images/Clear.png';
+import rain from '../../assets/images/Rain.png';
+import snow from '../../assets/images/Snow.png';
+import clouds from '../../assets/images/Clouds.png';
+import haze from '../../assets/images/Haze.png';
+import smoke from '../../assets/images/Smoke.png';
+import mist from '../../assets/images/Mist.png';
+import drizzle from '../../assets/images/Drizzle.png';
+import temp from '../../assets/images/temp.png';
+import noWifi from '../../assets/images/no-wifi.png';
+import loading from '../../assets/images/loading.png';
+import notFound from '../../assets/images/NotFound.png';
+
+
+
+const weatherTypes =
+    {
+        "temp": temp,
+        "Clear": clear,
+        "Rain": rain,
+        "Snow": snow,
+        "Clouds": clouds,
+        "Haze": haze,
+        "Smoke": smoke,
+        "Mist": mist,
+        "Drizzle": drizzle,
+        "loading": loading,
+        "noWifi": noWifi,
+        "notFound": notFound,
+    };
+
+export {weatherTypes};
