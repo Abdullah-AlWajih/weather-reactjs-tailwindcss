6961bf1d11a511f7b4c4203363f3256ab5a35648
Abdullah Al-Wajih <Pro.Abdullah.AlWajih@gmail.com>
2023-05-25 22:46:53 +0300

    add background image

--- src/App.js
+++ src/App.js
@@ -78,8 +78,8 @@ function App() {
 
 
     return (
-        <div className='bg-secondary min-h-screen justify-center flex items-center   px-8'>
-            <div className='bg-white drop-shadow-xl dark:bg-slate-800  w-96 p-4 rounded-md'>
+        <div className="bg-secondary bg-[url('./assets/weather.jpg')] min-h-screen justify-center flex items-center   px-8">
+            <div className='backdrop-blur-sm bg-white/10 drop-shadow-xl dark:bg-slate-800  w-96 p-4 rounded-md'>
                 <Input searchWeather={searchWeather} inputRef={inputRef}/>
                 <div className={`duration-300 grid place-items-center delay-75 overflow-hidden ${apiData || loading ? 'h-[27rem]' : 'h-0'} `}>
                     {
@@ -90,17 +90,17 @@ function App() {
                             apiData && (<div className='text-center flex flex-col gap-6 mt-10'>
                                 {
                                     apiData?.name && (
-                                        <p className='text-xl text-gray-900 font-semibold'>{apiData?.name + ', ' + apiData?.country} </p>)
+                                        <p className='text-xl  text-gray-100 font-semibold'>{apiData?.name + ', ' + apiData?.country} </p>)
                                 }
                                 <img src={apiData?.img} alt="" className='w-52 mx-auto'/>
-                                <h3 className='text-2xl text-gray-900  font-bold'>
+                                <h3 className='text-2xl text-gray-100  font-bold'>
                                     {apiData?.description}
                                 </h3>
                                 {
                                     apiData?.temp && (<div className='flex justify-center'>
-                                        <h2 className='text-4xl text-gray-900 font-extrabold'>{apiData?.temp}&#176;C</h2>
-                                        <img src={images.weatherTypes.temp} alt=""
-                                             className='h-9 mt-1'/>
+                                        <h2 className='text-4xl text-gray-300 font-extrabold'>{apiData?.temp}&#176;C</h2>
+                                        {/*<img src={images.weatherTypes.temp} alt=""*/}
+                                        {/*     className='h-9 fill-gray-100 mt-1'/>*/}
                                     </div>)
                                 }
                             </div>)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-      primary: "#FFCE71",
-      "primary-container": "#FCE3A0",
-      secondary: "#D4D4D5",
-      "secondary-container": '#EFEDEE',
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FFCE71",
+                "primary-container": "#FCE3A0",
+                secondary: "#D4D4D5",
+                "secondary-container": '#EFEDEE',
+                backgroundImage: {
+                    'hero': "url('./assets/bg.svg')",
+                }
+            },
+        },
     },
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
 
